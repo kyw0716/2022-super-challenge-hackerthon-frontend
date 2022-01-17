542c676274496318358e4f13e2720efbc311dafc
kyw0716 <kyw0716@naver.com>
2022-01-17 20:50:42 +0900

    hompage css modify
    홈페이지 바탕색 영역, 박스 크기, 버튼 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
-import style from "./Home.module.css"
+import styles from "./Home.module.css"
 
 function Home() {
 
     return (
         <div className="Home">
-            <div className={style.upBox}><button><Link to="/student">student</Link></button></div>
-            <div className={style.downBox}><button><Link to="/teacher">teacher</Link></button></div>
-            <div className={style.student}>
+            <div className={styles.upBox}><button><Link to="/student">student</Link></button></div>
+            <div className={styles.downBox}><button><Link to="/teacher">teacher</Link></button></div>
+            <div className={styles.student}>
                 <span>학생 / 학부모용</span>
                 <span>~~~~~~~~~~~~~~</span>
-                <button><Link to="/login">로그인 / 회원가입</Link></button>
+                <button className={styles.btn}><Link to="/login">로그인 / 회원가입</Link></button>
             </div>
-            <div className={style.teacher}>
+            <div className={styles.teacher}>
                 <span>교사 / 교직원용</span>
                 <span>~~~~~~~~~~~~~~</span>
-                <button><Link to="/login">로그인 / 회원가입</Link></button>
+                <button className={styles.btn}><Link to="/login">로그인 / 회원가입</Link></button>
             </div>
         </div>
     );
