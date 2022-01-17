000e3a629f10c2438bff17cd049564227c98ec26
kyw0716 <kyw0716@naver.com>
2022-01-17 23:53:08 +0900

    Add logo
    로고 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,10 @@ import styles from "./Home.module.css"
 function Home() {
     return (
         <div className={styles.Home}>
-            <div className={styles.upBox}><button><Link to="/student">student</Link></button></div>
+            <div className={styles.upBox}>
+                <span className={styles.hitech}>@ㅎㅏㅌㅔㄱㅇㅣㅁㅣㄹㅐㄷㅏㅇ</span><br/>
+                <button className={styles.studentBtn}><Link to="/student">student</Link></button>
+            </div>
             <div className={styles.downBox}><button><Link to="/teacher">teacher</Link></button></div>
             <HomeStudent text="학생 / 학부모용"/>
             <HomeTeacher text="교사 / 교직원용"/>
