bdaed48eb3cd4ef1fe77f7351183e646f9c71ebc
kyw0716 <kyw0716@naver.com>
2022-01-21 19:40:40 +0900

    self diagnosis
    자가진단 radio버튼 클릭 오류 수정

--- src/routes/List.js
+++ src/routes/List.js
@@ -1,10 +1,18 @@
 import style from "./List.module.css"
 
-function List({ q, setCon, number}) {
+function List({ q, setCon, setNumber, number}) {
     const Item = ({ text }) => {
         return text;
     }
+    const n = number + 1;
     const onChange = (event) => {
+        if(n === 4){
+            alert('제출버튼을 클릭해주세요!!');
+            setNumber(n - 1);
+        }
+        else{
+            setNumber(n);
+        }
         if(event.target.value === "yes"){
             setCon(true);
         }
@@ -13,18 +21,18 @@ function List({ q, setCon, number}) {
         }
     }
     return (
-        <div className={style.list} key={number}>
+        <div className={style.list}>
             <div className={style.question}>
                 <Item text={q} />
             </div>
             <div className="answer">
                 <label htmlFor="yes">예</label>
-                <input id="yes" type="radio" value="yes" name={number} onChange={onChange} />
+                <input id="yes" type="radio" value="yes" name={number} onClick={onChange} />
                 <label htmlFor="no">아니요</label>
-                <input id="no" type="radio" value="no" name={number} onChange={onChange} />
+                <input id="no" type="radio" value="no" name={number} onClick={onChange} />
             </div>
         </div>
     )
 }
 
-export default List; 
+export default List;
--- src/routes/Page.js
+++ src/routes/Page.js
@@ -5,12 +5,20 @@ import { useState } from "react";
 
 function Page() {
     const [con, setCon] = useState(false);
+    const [number, setNumber] = useState(0);
     const onSubmit = (e) => {
         e.preventDefault();
         console.log(con);
-        window.location.href="/studentPage";
+        if(number === 3){
+            window.location.href="/studentPage";
+        }
     }
-
+    const text = [
+        ["1. 귀하는 코로나 19가 의심되는 아래의 임상증상*이 있나요?", <br />, "*(주요 임상증상) 발열(37.5도 이상), 기침, 호흡곤란, 오한, 근육통, 두통, 인후통, 후각-미각소실", <br />, "※단 학교에서 선별진료소 검사결과(음성)을 확인 후 등교를 허용한 경우, 또는 선천성 질환-만성질환(천식 등)으로  인한 증상인 경우 '아니오'를 선택하시오."],
+        ["2. 귀하 본인 또는 동거인이 코로나19 진단검사를 받고 그 결과를 기다리고 있나요?", <br />, "※단 확진자와의 접촉 가능성 등으로 보건당국의 권고에 의해 검사한 것이 아닌경우 '아니오'를 선택하세요", <br />, "*직업특성에 의한 정기검사, 대회참여 등 선제적 예방 목적"],
+        ["3. 귀하 본인 또는 동거인이 방역당국에 의해 현재 자가격리가 이루어지고 있나요?", <br />, "※'단계적 일상회복을 위한 공무원 복무관리 지침'을 준용, 학교장과 협의하여 출근 판단"],
+        ["4. 귀하의 동거인 중 확진자가 있나요?", <br />, "※단 귀하가 방역당국 지침에 따라 수동감시 대상이 된 경우 '아니오'를 선택하세요", <br />, "*수동감시 요건: PCR검사 음성, 밀접접촉 당시 접종 완료, 무증상 등 모두 충족 <방역당국 접종 완료자 지침>"]
+    ]
     return (
         <div className={style.Page}>
             <div className={style.page}>
@@ -19,7 +27,7 @@ function Page() {
                     <Link className={style.back} to="/studentPage">처음으로</Link>
                 </div>
                 <form className={style.form}>
-                    <List  
+                    {/* <List  
                         number="1"
                         q={["1. 귀하는 코로나 19가 의심되는 아래의 임상증상*이 있나요?", <br />, "*(주요 임상증상) 발열(37.5도 이상), 기침, 호흡곤란, 오한, 근육통, 두통, 인후통, 후각-미각소실", <br />, "※단 학교에서 선별진료소 검사결과(음성)을 확인 후 등교를 허용한 경우, 또는 선천성 질환-만성질환(천식 등)으로  인한 증상인 경우 '아니오'를 선택하시오."]} setCon={setCon}
                     />
@@ -37,6 +45,12 @@ function Page() {
                         number="4" 
                         q={["4. 귀하의 동거인 중 확진자가 있나요?", <br />, "※단 귀하가 방역당국 지침에 따라 수동감시 대상이 된 경우 '아니오'를 선택하세요", <br />, "*수동감시 요건: PCR검사 음성, 밀접접촉 당시 접종 완료, 무증상 등 모두 충족 <방역당국 접종 완료자 지침>"]} 
                         setCon={setCon}
+                    /> */}
+                    <List
+                        q={text[number]}
+                        setCon={setCon}
+                        setNumber={setNumber}
+                        number={number}
                     />
                     <input type="button" className={style.submit} onClick={onSubmit} value="제출"/>
                 </form>
