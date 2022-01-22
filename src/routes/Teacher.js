41a85fd5892bb3779420335cfb185f773586b06d
sugamypapa <sk000801@naver.com>
2022-01-22 21:26:40 +0900

    CSS modify2
    teacher 페이지 + 제출 버튼 수정

--- src/routes/Teacher.js
+++ src/routes/Teacher.js
@@ -98,6 +98,11 @@ function Teacher() {
                     </div>
                     <div className={style.notice}>
                         공지사항
+                        <div className={style.Gongji}>
+                            <span> <span className={style.blue}>who?</span> 확진, 격리 통지받은 학생 </span>
+                            <span> <span className={style.blue}>when?</span> 보건당국의 입원치료 통지 또는 격리 통지 시부터 격리 해제시까지</span>
+                            <span> <span className={style.blue}>what?</span> 입원치료통지서, 격리통지서</span>   
+                        </div>
                     </div>
                     <div className={style.pointer} onClick={() => {window.location.href = "/guidePage"}}>
                         <Link className={style.point} to="/guidePage">
